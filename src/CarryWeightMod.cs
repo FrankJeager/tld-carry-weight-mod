13f3eb45d0f613e0472319fc0fd180af1b719aba
FrankJeager <106985437+FrankJeager@users.noreply.github.com>
2024-06-27 10:02:22 +0200

    Fix for TLD 2.29

--- src/CarryWeightMod.cs
+++ src/CarryWeightMod.cs
@@ -1,7 +1,8 @@
-﻿using MelonLoader;
+using MelonLoader;
 using Il2Cpp;
 using Il2CppInterop.Runtime.Injection;
 using Il2CppTLD.Gear;
+using Il2CppTLD.IntBackedUnit;
 using static Il2CppSystem.Xml.XmlWellFormedWriter.AttributeValueCache;
 
 namespace CarryWeightMod
@@ -20,23 +21,23 @@ namespace CarryWeightMod
                 int carryAdd = Settings.options.carryKgAdd;
                 if (Settings.options.infiniteCarry) carryAdd = 9970;
 
-                encumberComp.m_MaxCarryCapacityKG = 30f + carryAdd;
-                encumberComp.m_MaxCarryCapacityWhenExhaustedKG = 15f + carryAdd;
-                encumberComp.m_NoSprintCarryCapacityKG = 40f + carryAdd;
-                encumberComp.m_NoWalkCarryCapacityKG = 60f + carryAdd;
-                encumberComp.m_EncumberLowThresholdKG = 31f + carryAdd;
-                encumberComp.m_EncumberMedThresholdKG = 40f + carryAdd;
-                encumberComp.m_EncumberHighThresholdKG = 60f + carryAdd;
+                encumberComp.m_MaxCarryCapacity = ItemWeight.FromKilograms(30f + carryAdd);
+                encumberComp.m_MaxCarryCapacityWhenExhausted = ItemWeight.FromKilograms(15f + carryAdd);
+                encumberComp.m_NoSprintCarryCapacity = ItemWeight.FromKilograms(40f + carryAdd);
+                encumberComp.m_NoWalkCarryCapacity = ItemWeight.FromKilograms(60f + carryAdd);
+                encumberComp.m_EncumberLowThreshold = ItemWeight.FromKilograms(31f + carryAdd);
+                encumberComp.m_EncumberMedThreshold = ItemWeight.FromKilograms(40f + carryAdd);
+                encumberComp.m_EncumberHighThreshold = ItemWeight.FromKilograms(60f + carryAdd);
             }
             else
             {
-                encumberComp.m_MaxCarryCapacityKG = 30f;
-                encumberComp.m_MaxCarryCapacityWhenExhaustedKG = 15f;
-                encumberComp.m_NoSprintCarryCapacityKG = 40f;
-                encumberComp.m_NoWalkCarryCapacityKG = 60f;
-                encumberComp.m_EncumberLowThresholdKG = 31f;
-                encumberComp.m_EncumberMedThresholdKG = 40f;
-                encumberComp.m_EncumberHighThresholdKG = 60f;
+                encumberComp.m_MaxCarryCapacity = ItemWeight.FromKilograms(30f);
+                encumberComp.m_MaxCarryCapacityWhenExhausted = ItemWeight.FromKilograms(15f);
+                encumberComp.m_NoSprintCarryCapacity = ItemWeight.FromKilograms(40f);
+                encumberComp.m_NoWalkCarryCapacity = ItemWeight.FromKilograms(60f);
+                encumberComp.m_EncumberLowThreshold = ItemWeight.FromKilograms(31f);
+                encumberComp.m_EncumberMedThreshold = ItemWeight.FromKilograms(40f);
+                encumberComp.m_EncumberHighThreshold = ItemWeight.FromKilograms(60f);
             }
         }
     }
